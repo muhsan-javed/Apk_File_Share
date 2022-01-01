5e52ef83fbe89c86def4d391f426fe962b7a091f
Muhsan <muhsantech@outlook.com>
2022-01-01 17:04:02 +0500

    Initial commit completed Error free app

---
+++ app/src/main/java/com/muhsantech/apkfileshare/AppAdapter.java
@@ -0,0 +1,102 @@
+package com.muhsantech.apkfileshare;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.FileProvider;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.File;
+import java.util.List;
+
+public class AppAdapter extends RecyclerView.Adapter<AppAdapter.AppViewHolder>{
+    Context context;
+    List<App> apps;
+
+    public AppAdapter(Context context, List<App> apps) {
+        this.context = context;
+        this.apps = apps;
+    }
+
+    @NonNull
+    @Override
+    public AppViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.app_row,parent,false);
+        return new AppViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AppViewHolder holder, @SuppressLint("RecyclerView") int position) {
+
+        holder.appName.setText(apps.get(position).getName());
+        long appSize = apps.get(position).getApkSize();
+        holder.apkSize.setText(getHumanReadableSize(appSize));
+
+        holder.appIcon.setImageDrawable(apps.get(position).getIcon());
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent shareApkIntent = new Intent();
+                shareApkIntent.setAction(Intent.ACTION_SEND);
+
+                shareApkIntent.putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(
+                        context,BuildConfig.APPLICATION_ID + ".provider", new File(apps.get(position).getApkPath())));
+
+                shareApkIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                shareApkIntent.setType("application/vnd.android.package-archive");
+
+                context.startActivity(Intent.createChooser(shareApkIntent,"Share APK"));
+
+            }
+        });
+    }
+
+    private String getHumanReadableSize(long apkSize) {
+
+        String humanReadableSize;
+        if (apkSize<1024){
+            humanReadableSize = String.format(context.getString(R.string.app_size_b),(double) apkSize);
+        }else if (apkSize < Math.pow(1024,2)){
+            humanReadableSize = String.format(context.getString(R.string.app_size_Kib),(double) (apkSize/1024));
+        }else if (apkSize < Math.pow(1024,3)){
+            humanReadableSize = String.format(context.getString(R.string.app_size_mib),(double) (apkSize/Math.pow(1024,2)));
+        }else {
+            humanReadableSize = String.format(context.getString(R.string.app_size_gib),(double) (apkSize/Math.pow(1024,3)));
+        }
+
+        return humanReadableSize;
+    }
+
+    @Override
+    public int getItemCount() {
+        return apps.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    public static class AppViewHolder extends RecyclerView.ViewHolder{
+        CardView cardView;
+        ImageView appIcon;
+        TextView appName, apkSize;
+        public AppViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardView = itemView.findViewById(R.id.app_row);
+            appIcon = itemView.findViewById(R.id.app_icon);
+            appName = itemView.findViewById(R.id.app_name);
+            apkSize = itemView.findViewById(R.id.apk_size);
+        }
+    }
+}
---
+++ app/src/main/java/com/muhsantech/apkfileshare/MainActivity.java
@@ -0,0 +1,68 @@
+package com.muhsantech.apkfileshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+
+import javax.crypto.AEADBadTagException;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<App> apps = new ArrayList<>();
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.app_list);
+
+        // Get App List
+        PackageManager pm = getApplicationContext().getPackageManager();
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for(ApplicationInfo packageInfo : packages){
+
+            String name;
+
+            if ((name = String.valueOf(pm.getApplicationLabel(packageInfo))).isEmpty()){
+                name = packageInfo.packageName;
+            }
+
+            Drawable icon = pm.getApplicationIcon(packageInfo);
+            String apkPath = packageInfo.sourceDir;
+            long apkSize = new File(packageInfo.sourceDir).length();
+
+            apps.add(new App(name, icon, apkPath, apkSize));
+
+        }
+
+        Collections.sort(apps, new Comparator<App>() {
+            @Override
+            public int compare(App app, App t1) {
+                return app.getName().toLowerCase().compareTo(t1.getName().toLowerCase());
+            }
+        });
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        AppAdapter appAdapter = new AppAdapter(this,apps);
+        recyclerView.setAdapter(appAdapter);
+
+    }
+}
